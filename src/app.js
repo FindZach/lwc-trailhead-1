7db7c416a8f2dc30dfbeccd3251db810f0723ab7
Zach <55464762+undefined@users.noreply.github.com>
2022-11-03 06:41:16 +0000

    accordion LWC

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
 import { LightningElement } from 'lwc';
 export default class App extends LightningElement {
-  message = 'Hello World';
+  activeSectionMessage = '';
+
+    handleToggleSection(event) {
+        this.activeSectionMessage =
+            'Open section name:  ' + event.detail.openSections;
+    }
+
+    handleSetActiveSectionC() {
+        const accordion = this.template.querySelector('.example-accordion');
+
+        accordion.activeSectionName = 'C';
+    }
 }
